52767dfb9df2f03628df5c86732fc8d0379ebe60
Jared Hoberock <jaredhoberock@gmail.com>
2020-05-04 15:23:15 -0500

    Introduce allocator property

--- cudex/property/allocator.hpp
+++ cudex/property/allocator.hpp
@@ -26,10 +26,65 @@
 
 #pragma once
 
-#include "detail/prologue.hpp"
+#include "../detail/prologue.hpp"
 
-#include "property/allocator.hpp"
-#include "property/bulk_guarantee.hpp"
+#if __has_include(<any>)
+#include <any>
+#endif
 
-#include "detail/epilogue.hpp"
+#include "detail/basic_executor_property.hpp"
+
+
+CUDEX_NAMESPACE_OPEN_BRACE
+
+
+template<class ProtoAllocator>
+class allocator_t : 
+  detail::basic_executor_property<
+    allocator_t<ProtoAllocator>,
+    true,
+    true
+#if __cpp_lib_any
+    , std::any
+#endif
+  >
+{
+  public:
+    CUDEX_ANNOTATION
+    constexpr explicit allocator_t(const ProtoAllocator& alloc) : alloc_(alloc) {}
+
+    CUDEX_ANNOTATION
+    constexpr ProtoAllocator value() const
+    {
+      return alloc_;
+    }
+
+  private:
+    ProtoAllocator alloc_;
+};
+
+
+template<>
+struct allocator_t<void> :
+  detail::basic_executor_property<
+    allocator_t<void>,
+    true,
+    true
+  >
+{
+  template<class ProtoAllocator>
+  CUDEX_ANNOTATION
+  constexpr allocator_t<ProtoAllocator> operator()(const ProtoAllocator& alloc) const
+  {
+    return allocator_t<ProtoAllocator>{alloc};
+  }
+};
+
+
+static constexpr allocator_t<void> allocator{};
+
+
+CUDEX_NAMESPACE_CLOSE_BRACE
+
+#include "../detail/epilogue.hpp"
 
